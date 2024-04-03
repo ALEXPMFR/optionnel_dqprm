9707f40acb7125dddc8cb589f75fc05bfdd7d63e
Alexandre Rintaud <alexandre.rintaud@gmail.com>
2024-04-03 11:08:40 +0200

    meilleur design suv.py

--- suv.py
+++ suv.py
@@ -1,9 +1,13 @@
 import re
 
-with open('20200527152905.861802.ig.tum', 'r') as fic:
-    txt = fic.read()
+def do_it():
+    with open('20200527152905.861802.ig.tum', 'r') as fic:
+        txt = fic.read()
 
-suv = re.findall(r'(M.+)(?<!RC)SUVValue = (\d+\.\d+)', txt)
+    suv = re.findall(r'(M.+)(?<!RC)SUVValue = (\d+\.\d+)', txt)
 
-d = {el[0]: float(el[1]) for el in suv}
-print(d)
+    d = {el[0]: float(el[1]) for el in suv}
+    print(d)
+
+if __name__ == '__main__':
+    do_it()
