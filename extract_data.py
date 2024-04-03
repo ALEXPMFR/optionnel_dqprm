3ccf0ea340623b6fb68c24bcb542cab848595e3e
Alexandre Rintaud <alexandre.rintaud@gmail.com>
2024-04-03 14:18:23 +0200

    ajout fonction fichiers de sortie SUV et HU

--- extract_data.py
+++ extract_data.py
@@ -1,5 +1,6 @@
 import re
 import argparse
+import csv
 
 def parse_fic():
     parser = argparse.ArgumentParser()
@@ -15,12 +16,23 @@ def ouverture_fichier():
         txt = fic.read()
     return txt
 
+def sortie_resultat(data):
+    return [(el[0], float(el[1])) for el in data]
+
+def csv_file(rows, fichier_out):
+    with open(fichier_out, 'w') as csv_out:
+        csv_output = csv.writer(csv_out)
+        for row in rows:
+            csv_output.writerow(row)
+
 def do_it():
     txt = ouverture_fichier()
     suv = extract_info(txt, 'SUV')
     hu = extract_info(txt, 'HU')
-    dic_suv = {el[0]: float(el[1]) for el in suv}
-    dic_hu = {el[0]: float(el[1]) for el in hu}
+    dic_suv = sortie_resultat(suv)
+    csv_file(dic_suv, 'output_suv.csv')
+    dic_hu = sortie_resultat(hu)
+    csv_file(dic_hu, 'output_hu.csv')
     print("SUV :\t", dic_suv)
     print("HU  :\t", dic_hu)
 
