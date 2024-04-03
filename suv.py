01c57b04d57a8837b02a5fdad002b269fb55d792
Alexandre Rintaud <alexandre.rintaud@gmail.com>
2024-04-03 09:59:58 +0200

    premier commit

---
+++ suv.py
@@ -0,0 +1,9 @@
+import re
+
+with open('data/20200527152905.861802.ig.tum', 'r') as fic:
+    txt = fic.read()
+
+suv = re.findall(r'(M.+)(?<!RC)SUVValue = (\d+\.\d+)', txt)
+
+d = {el[0]: float(el[1]) for el in suv}
+print(d)
