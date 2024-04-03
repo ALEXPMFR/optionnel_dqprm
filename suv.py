8f46223f58e83a5ea7c8e3c7ec9313d233e05e72
Alexandre Rintaud <alexandre.rintaud@gmail.com>
2024-04-03 12:08:58 +0200

    ajout extraction SUV dans une fonction à part

--- suv.py
+++ suv.py
@@ -6,12 +6,14 @@ def parse_fic():
     parser.add_argument('fichier') # ajout argument positionnel
     return parser.parse_args()
 
+def extract_SUV(chaine):
+    suv = re.findall(r'(M.+)(?<!RC)SUVValue = (\d+\.\d+)', chaine)
+    return suv
+
 def do_it():
     with open(parse_fic().fichier, 'r') as fic:
         txt = fic.read()
-
-    suv = re.findall(r'(M.+)(?<!RC)SUVValue = (\d+\.\d+)', txt)
-
+    suv = extract_SUV(txt)
     d = {el[0]: float(el[1]) for el in suv}
     print(d)
 
