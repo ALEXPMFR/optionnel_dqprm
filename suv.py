fb181ec60d3d9c51f9ea5f05785a01bb8b69e2f0
Alexandre Rintaud <alexandre.rintaud@gmail.com>
2024-04-03 13:31:31 +0200

    design code pour rassembler les extractions SUV et HU dans une même fonction

--- suv.py
+++ suv.py
@@ -6,16 +6,23 @@ def parse_fic():
     parser.add_argument('fichier') # ajout argument positionnel
     return parser.parse_args()
 
-def extract_SUV(chaine):
-    suv = re.findall(r'(M.+)(?<!RC)SUVValue = (\d+\.\d+)', chaine)
-    return suv
+def extract_info(chaine, type_info):
+    info = re.findall(r'(M.+)(?<!RC)' + type_info + r'Value = (\d+(\.\d+)?)', chaine)
+    return info
 
-def do_it():
+def ouverture_fichier():
     with open(parse_fic().fichier, 'r') as fic:
         txt = fic.read()
-    suv = extract_SUV(txt)
-    d = {el[0]: float(el[1]) for el in suv}
-    print(d)
+    return txt
+
+def do_it():
+    txt = ouverture_fichier()
+    suv = extract_info(txt, 'SUV')
+    hu = extract_info(txt, 'HU')
+    dic_suv = {el[0]: float(el[1]) for el in suv}
+    dic_hu = {el[0]: float(el[1]) for el in hu}
+    print("SUV :\t", dic_suv)
+    print("HU  :\t", dic_hu)
 
 if __name__ == '__main__':
     do_it()
